fc721f7986ce7877c3c3438e69469f6bc2c358c0
srijareddy <srijareddyp@gmail.com>
2016-04-21 05:12:06 +0000

    Con1

--- mongo_con.js
+++ mongo_con.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
+var prompt = require('prompt')
 var converter = require('csvtojson').Converter;
 var fs = require('fs');
 
-
-mongoose.connect(' mongodb://srija:srija@ds013901.mlab.com:13901/heroku_r1s7tdsw');
+mongoose.connect('mongodb://srija:srija@ds013901.mlab.com:13901/heroku_r1s7tdsw')
 var conn = mongoose.connection;
 
 conn.on('error', function(err){
@@ -23,5 +23,12 @@ fs.createReadStream(csvfile).pipe(csv_con);
 
 csv_con.on("end_parsed", function(jsonObj){
     
-    conn.collection('sample').insert(jsonObj);
+    conn.collection('test').insert(jsonObj);
     }); 
+
+
+console.log('Collection created and data inserted successfully...');
+var Schema = mongoose.Schema;
+var Weather_schema = new Schema({ID : Number, TEMP : Number, PRES : Number, DEWP : Number});
+
+var weather = mongoose.model('weather','Weather_schema' ,'weathers');
