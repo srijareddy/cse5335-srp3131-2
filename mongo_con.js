c78e07a805b3cc4343e97a027fa402182e0ffd9e
srijareddy <srijareddyp@gmail.com>
2016-04-21 03:48:53 +0000

    Connections

---
+++ mongo_con.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var converter = require('csvtojson').Converter;
+var fs = require('fs');
+
+mongoose.connect('mongodb://srija:srija@ds013901.mlab.com:13901/heroku_r1s7tdsw');
+var conn = mongoose.connection;
+
+conn.on('error', function(err){
+    
+    console.log('Connection error', err);
+    });
+conn.once('open', function(){
+    
+    //console.log('Connected...');
+    });
+
+var csvfile = 'sample.csv';
+var csv_con = new converter();
+
+fs.createReadStream(csvfile).pipe(csv_con);
+
+
+csv_con.on("end_parsed", function(jsonObj){
+    
+    conn.collection('weather').insert(jsonObj);
+    }); 
+
+
+console.log('Collection created and data inserted successfully...');
