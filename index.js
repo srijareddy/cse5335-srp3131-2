8ad775336bd9e6fd8b52c732284a1e0ee813e7ba
srijareddy <srijareddyp@gmail.com>
2016-04-21 06:05:26 +0000

    code

--- index.js
+++ index.js
@@ -16,5 +16,36 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+app.get('/p/:tagId', function(req, res) {
+  var i= req.params.tagId;
+  i = parseInt(i);
+  //connection code
+  
+  var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://srija:srija@ds013901.mlab.com:13901/heroku_r1s7tdsw",function(err, db){
+        if(err) throw err;
+				
+	    db.collection('weathers').findOne({ID:i}, function(err, doc){
+        if(err) throw err;
+        console.log("Server:"+doc);
+		
+		res.json(doc)
+	    db.close();
+     });	 
+});
+
+  //connection code
+
+  
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+
 
 
